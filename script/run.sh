2266e230b31ab4cce2c9bbda90ac55fd5fcd68fa
Giulia Bianchi <giulbia@gmail.com>
2019-08-04 22:21:27 +0200

    update docker image

--- script/run.sh
+++ script/run.sh
@@ -1,2 +1,1 @@
-docker run --rm --device /dev/snd -v `pwd`:/opt/baby_cry_rpi -w /opt/baby_cry_rpi -it mikkl/rpi-raspbian-baby-cry-detection:pi2 bash script/run_crying_detection.sh
-
+docker run --rm --device /dev/snd -v `pwd`:/opt/baby_cry_rpi -w /opt/baby_cry_rpi -it mikkl/rpi2-babycry:squashed bash script/run_crying_detection.sh
